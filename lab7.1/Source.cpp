483d46c56296d81eeeb5f8cd34e517b991ae6246
viktoriiazz <viktoriia.zborovska@gmail.com>
2022-12-22 23:50:51 +0200

    Add project files.

---
+++ lab7.1/Source.cpp
@@ -0,0 +1,96 @@
+
+// Lab_7_1.cpp
+// <Зборовська ВІкторія >
+// Лабораторна робота № 7.1.
+// Пошук заданих елементів та впорядкування рядків / стовпчиків матриці.
+// Варіант 9
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+	const int High);
+void Print(int** a, const int rowCount, const int colCount);
+void Sort(int** a, const int rowCount, const int colCount);
+void Change(int** a, const int col1, const int col2, const int rowCount);
+void Calc(int** a, const int rowCount, const int colCount, int& S, int& k);
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = -6;
+	int High = 31;
+	int rowCount = 7;
+	int colCount = 5;
+	int** a = new int* [rowCount];
+	for (int i = 0; i < rowCount; i++)
+		a[i] = new int[colCount];
+	Create(a, rowCount, colCount, Low, High);
+	Print(a, rowCount, colCount);
+	Sort(a, rowCount, colCount);
+	Print(a, rowCount, colCount);
+	int S = 0;
+	int k = 0;
+	Calc(a, rowCount, colCount, S, k);
+	cout << "S = " << S << endl;
+	cout << "k = " << k << endl;
+	Print(a, rowCount, colCount);
+	for (int i = 0; i < rowCount; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+	const int High)
+{
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			a[i][j] = Low + rand() % (High - Low + 1);
+}
+void Print(int** a, const int rowCount, const int colCount)
+{
+	cout << endl;
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < colCount; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+void Sort(int** a, const int rowCount, const int colCount)
+{
+	for (int i0 = 0; i0 < rowCount - 1; i0++)
+		for (int i1 = 0; i1 < rowCount - i0 - 1; i1++)
+			if ((a[i1][0] < a[i1 + 1][0])
+				||
+				(a[i1][0] == a[i1 + 1][0] &&
+					a[i1][1] < a[i1 + 1][1])
+				||
+				(a[i1][0] == a[i1 + 1][0] &&
+					a[i1][1] == a[i1 + 1][1] &&
+					a[i1][3] > a[i1 + 1][3]))
+				Change(a, i1, i1 + 1, colCount);
+}
+void Change(int** a, const int col1, const int col2, const int rowCount)
+{
+	int tmp;
+	for (int j = 0; j < rowCount; j++)
+	{
+		tmp = a[col1][j];
+		a[col1][j] = a[col2][j];
+		a[col2][j] = tmp;
+	}
+}
+void Calc(int** a, const int rowCount, const int colCount, int& S, int& k)
+{
+	S = 0;
+	k = 0;
+	for (int i = 0; i < rowCount; i++)
+		for (int j = 0; j < colCount; j++)
+			if (a[i][j] >= 0 && !(a[i][j] % 3 == 0))
+			{
+				S += a[i][j];
+				k++;
+				a[i][j] = 0;
+			}
+}
